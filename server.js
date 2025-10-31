0dd73329eaaded29932d04bed99c36f335335244
delacruzsamb@gmail.com <121100632+samueldelacruz@users.noreply.github.com>
2025-10-31 10:45:32 +0800

    Added routes with index.js

--- server.js
+++ server.js
@@ -1,1 +1,9 @@
-console.log("Hello World!");
+const express = require('express');
+const app = express();
+
+const port = process.env.port || 3000;
+
+// Routes
+app.use('/', require('./routes'));
+
+app.listen(port, () => {console.log(`Server is running on port: http://localhost:${port}`)}); 
