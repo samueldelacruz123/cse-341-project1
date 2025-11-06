64dce93758387da58a12df23e8a26c8e2468f69c
delacruzsamb@gmail.com <121100632+samueldelacruz@users.noreply.github.com>
2025-11-06 16:20:31 +0800

    Project: Contacts Part 2 Completed

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const router = express.Router();
-
 const contactsController = require('../controllers/contacts');
 
-router.get('/', contactsController.getAll);
+router.get('/', contactsController.getAllContact);
 
-router.get('/:id', contactsController.getSingle);
+router.get('/:id', contactsController.getSingleContact);
+
+router.post('/', contactsController.createContact);
+
+router.put('/:id', contactsController.updateContact);
+
+router.delete('/:id', contactsController.deleteContact);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 const express = require('express');
-
 const mongodb = require('./data/database');
 const app = express();
-
+const bodyParser = require('body-parser');
 const port = process.env.port || 3000;
+
+app.use(bodyParser.json());
+
+// Swagger setup
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers', 
+    'Origin, X-Requested-With, Content-Type, Accept, Z-key'
+  );
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  next();
+});
 
 // Routes
 app.use('/', require('./routes'));
